47702bcef9b65164fb95d0cc646cd071ea9e370d
João Victor Carvalho <carvalhojoaovi20@gmail.com>
2023-03-01 17:50:21 -0300

    feat: add taskdetails back button

--- src/components/taskDetails/TaskDetails.js
+++ src/components/taskDetails/TaskDetails.js
@@ -2,19 +2,25 @@ import React from 'react';
 import { Button } from '../button/Button';
 import { useParams } from 'react-router-dom';
 import "./TaskDetails.css";
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
 
 
 
 const TaskDetails = () => {
     const params = useParams();
+    const history = useHistory();
+
+    const handleBackButtonClick = () => {
+        history.goBack();
+    }
     
     return (
 
 
         <>
         <div className='back-button-container'>
-            <Button>Back</Button>
+            <Button onClick={handleBackButtonClick}>Back</Button>
 
         </div>
         <div className='task-details-container'>
